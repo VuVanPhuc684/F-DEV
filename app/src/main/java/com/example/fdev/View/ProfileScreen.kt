98f140d45210eabc70eafe01916dc1b06f09e994
Vu Van Phuc <phucvuvan63@gmail.com>
2024-10-14 07:54:19 +0700

    gán dữ liệu từ firebase lên tên và email ở ProfileScreen

--- app/src/main/java/com/example/fdev/View/ProfileScreen.kt
+++ app/src/main/java/com/example/fdev/View/ProfileScreen.kt
@@ -1,22 +1,260 @@
+//package com.example.fdev.View
+//
+//import android.service.quicksettings.Tile
+//import androidx.compose.foundation.Image
+//import androidx.compose.foundation.background
+//import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Row
+//import androidx.compose.foundation.layout.Spacer
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.layout.size
+//import androidx.compose.foundation.lazy.LazyColumn
+//import androidx.compose.foundation.rememberScrollState
+//import androidx.compose.foundation.shape.RoundedCornerShape
+//import androidx.compose.material3.AlertDialog
+//import androidx.compose.material3.Card
+//import androidx.compose.material3.Text
+//import androidx.compose.material3.TextButton
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.runtime.setValue
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.draw.clip
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.layout.ContentScale
+//import androidx.compose.ui.platform.LocalContext
+//import androidx.compose.ui.res.painterResource
+//import androidx.compose.ui.text.font.FontFamily
+//import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.unit.sp
+//import androidx.navigation.NavController
+//import androidx.navigation.compose.rememberNavController
+//import com.example.fdev.R
+//import com.example.fdev.ViewModel.data.setingItem
+//import com.example.fdev.model.Profile
+//import com.google.firebase.auth.FirebaseAuth
+//
+//@Composable
+//fun ProfileScreen(navController: NavController) {
+//    var showLogoutDialog = remember { mutableStateOf(false) } // Trạng thái hiển thị hộp thoại
+//// Firebase Authentication instance to get the current user
+//    val auth = FirebaseAuth.getInstance()
+//    val currentUser = auth.currentUser
+//
+//    // Auto-fill user's name and email from Firebase
+//    var name by remember { mutableStateOf(currentUser?.displayName ?: "") }
+//    var email by remember { mutableStateOf(currentUser?.email ?: "") }
+//    var noiDung by remember { mutableStateOf("") }
+//
+//    val scrollState = rememberScrollState()
+//    val context = LocalContext.current
+//
+//
+//    Column(
+//        modifier = Modifier
+//            .padding(20.dp)
+//            .fillMaxWidth(),
+//        verticalArrangement = Arrangement.Top,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        Row(
+//            modifier = Modifier.fillMaxWidth(),
+//            horizontalArrangement = Arrangement.SpaceBetween,
+//            verticalAlignment = Alignment.CenterVertically
+//        ) {
+//            Image(
+//                painterResource(id = R.drawable.search),
+//                contentDescription = null,
+//                modifier = Modifier
+//                    .size(25.dp)
+//                    .clickable { /* su kien onClick */ },
+//                contentScale = ContentScale.FillBounds
+//            )
+//            Text(
+//                text = "PROFILE",
+//                modifier = Modifier.align(Alignment.CenterVertically),
+//                fontSize = 24.sp,
+//                fontWeight = FontWeight.Medium,
+//                fontFamily = FontFamily.Serif,
+//            )
+//            Image(
+//                painterResource(id = R.drawable.log_out),
+//                contentDescription = null,
+//                modifier = Modifier
+//                    .size(25.dp)
+//                    .clickable { showLogoutDialog.value = true }, // Hiển thị hộp thoại khi ấn vào
+//                contentScale = ContentScale.FillBounds
+//            )
+//        }
+//        Spacer(modifier = Modifier.height(15.dp))
+//
+//        TopNotifi()
+//
+//        LazyColumn {
+//            items(setingItem.size) { index ->
+//                ItemSeting1(setingItem[index], navController)
+//            }
+//        }
+//
+//        // Hiển thị hộp thoại đăng xuất
+//        if (showLogoutDialog.value) {
+//            LogoutConfirmationDialog(
+//                onConfirm = {
+//                    showLogoutDialog.value = false
+//                    navController.navigate("LOGIN") // Điều hướng đến màn hình LayoutLoginScreen
+//                },
+//                onDismiss = {
+//                    showLogoutDialog.value = false // Ẩn hộp thoại nếu ấn hủy
+//                }
+//            )
+//        }
+//    }
+//}
+//
+//@Composable
+//fun TopNotifi() {
+//    Column(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(10.dp),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Top
+//    ) {
+//        Row(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(top = 10.dp)
+//        ) {
+//            Image(
+//                painterResource(id = R.drawable.avatar_test),
+//                contentDescription = "avatar",
+//                modifier = Modifier
+//                    .size(80.dp)
+//                    .clip(RoundedCornerShape(50.dp))
+//            )
+//            Column(
+//                modifier = Modifier
+//                    .padding(start = 20.dp, top = 15.dp),
+//                horizontalAlignment = Alignment.Start,
+//                verticalArrangement = Arrangement.Center
+//            ) {
+//                Text(
+//                    text = name,
+//                    fontSize = 20.sp,
+//                    fontWeight = FontWeight.Bold
+//                )
+//                Text(
+//                    text = email,
+//                    fontSize = 14.sp,
+//                    modifier = Modifier
+//                        .padding(top = 5.dp)
+//                )
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun ItemSeting1(item: Profile, navController: NavController) {
+//    Card(
+//        modifier = Modifier
+//            .padding(10.dp)
+//            .clip(RoundedCornerShape(10.dp))
+//            .fillMaxWidth()
+//            .height(80.dp)
+//            .background(Color.LightGray)
+//            .clickable {
+//                navController.navigate(item.route)  // Điều hướng theo route của từng item
+//            }
+//    ) {
+//        Row(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(10.dp),
+//            horizontalArrangement = Arrangement.SpaceBetween,
+//            verticalAlignment = Alignment.CenterVertically
+//        ) {
+//            Column(
+//                modifier = Modifier
+//                    .padding(5.dp),
+//                verticalArrangement = Arrangement.Center,
+//                horizontalAlignment = Alignment.Start
+//            ) {
+//                Text(
+//                    text = item.title,
+//                    fontSize = 18.sp,
+//                    fontWeight = FontWeight.Medium
+//                )
+//                Text(
+//                    text = item.content)
+//            }
+//            Image(
+//                painterResource(id = item.iconResId),
+//                contentDescription = null,
+//                modifier = Modifier.size(25.dp),
+//                contentScale = ContentScale.FillBounds,
+//            )
+//        }
+//        Spacer(modifier = Modifier.height(15.dp))
+//    }
+//}
+//
+//@Composable
+//fun LogoutConfirmationDialog(onConfirm: () -> Unit, onDismiss: () -> Unit) {
+//    AlertDialog(
+//        onDismissRequest = { onDismiss() },
+//        title = {
+//            Text(text = "Xác nhận đăng xuất")
+//        },
+//        text = {
+//            Text("Bạn có chắc muốn đăng xuất không?")
+//        },
+//        confirmButton = {
+//            TextButton(onClick = { onConfirm() }) {
+//                Text("Có")
+//            }
+//        },
+//        dismissButton = {
+//            TextButton(onClick = { onDismiss() }) {
+//                Text("Không")
+//            }
+//        }
+//    )
+//}
+//
+//@Preview(showBackground = true, showSystemUi = true)
+//@Composable
+//fun PreviewProfile() {
+//    var navController = rememberNavController()
+//    ProfileScreen(navController)
+//}
 package com.example.fdev.View
 
-import android.service.quicksettings.Tile
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,18 +263,24 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.fdev.R
 import com.example.fdev.ViewModel.data.setingItem
 import com.example.fdev.model.Profile
-
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun ProfileScreen(navController: NavController) {
+    var showLogoutDialog = remember { mutableStateOf(false) } // Trạng thái hiển thị hộp thoại
+    val auth = FirebaseAuth.getInstance()
+    val currentUser = auth.currentUser
+
+    // Auto-fill user's name and email from Firebase
+    var name by remember { mutableStateOf(currentUser?.displayName ?: "Unknown") }
+    var email by remember { mutableStateOf(currentUser?.email ?: "Unknown") }
+
     Column(
         modifier = Modifier
             .padding(20.dp)
@@ -54,7 +298,7 @@ fun ProfileScreen(navController: NavController) {
                 contentDescription = null,
                 modifier = Modifier
                     .size(25.dp)
-                    .clickable { /* su kien onClick */ },
+                    .clickable { /* sự kiện onClick */ },
                 contentScale = ContentScale.FillBounds
             )
             Text(
@@ -69,24 +313,37 @@ fun ProfileScreen(navController: NavController) {
                 contentDescription = null,
                 modifier = Modifier
                     .size(25.dp)
-                    .clickable { },
+                    .clickable { showLogoutDialog.value = true }, // Hiển thị hộp thoại khi ấn vào
                 contentScale = ContentScale.FillBounds
             )
         }
         Spacer(modifier = Modifier.height(15.dp))
 
-        TopNotifi()
+        TopNotifi(name = name, email = email)
 
         LazyColumn {
             items(setingItem.size) { index ->
                 ItemSeting1(setingItem[index], navController)
             }
         }
+
+        // Hiển thị hộp thoại đăng xuất
+        if (showLogoutDialog.value) {
+            LogoutConfirmationDialog(
+                onConfirm = {
+                    showLogoutDialog.value = false
+                    navController.navigate("LOGIN") // Điều hướng đến màn hình LayoutLoginScreen
+                },
+                onDismiss = {
+                    showLogoutDialog.value = false // Ẩn hộp thoại nếu ấn hủy
+                }
+            )
+        }
     }
 }
 
 @Composable
-fun TopNotifi() {
+fun TopNotifi(name: String, email: String) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -113,12 +370,12 @@ fun TopNotifi() {
                 verticalArrangement = Arrangement.Center
             ) {
                 Text(
-                    text = "Nguyễn Anh Tuấn",
+                    text = name,  // Hiển thị tên người dùng
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold
                 )
                 Text(
-                    text = "Tuanna24@gmail.com",
+                    text = email,  // Hiển thị email người dùng
                     fontSize = 14.sp,
                     modifier = Modifier
                         .padding(top = 5.dp)
@@ -127,6 +384,7 @@ fun TopNotifi() {
         }
     }
 }
+
 @Composable
 fun ItemSeting1(item: Profile, navController: NavController) {
     Card(
@@ -172,10 +430,25 @@ fun ItemSeting1(item: Profile, navController: NavController) {
     }
 }
 
-
-@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun PreviewProfile() {
-    var navController = rememberNavController()
-    ProfileScreen(navController)
+fun LogoutConfirmationDialog(onConfirm: () -> Unit, onDismiss: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = { onDismiss() },
+        title = {
+            Text(text = "Xác nhận đăng xuất")
+        },
+        text = {
+            Text("Bạn có chắc muốn đăng xuất không?")
+        },
+        confirmButton = {
+            TextButton(onClick = { onConfirm() }) {
+                Text("Có")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { onDismiss() }) {
+                Text("Không")
+            }
+        }
+    )
 }
